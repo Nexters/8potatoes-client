642ff74b9eec433fa7fc168fb6c4049ca481fd5e
RookieAND <gwangin1999@naver.com>
2024-07-13 21:54:43 +0900

    feat: Vite Config 에 Absolute Path 관련 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: [{ find: '#/', replacement: '/src/' }],
+    },
     plugins: [react()],
 });
