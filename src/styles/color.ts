bec73100850c20f2017fc16fbcceaa24705568be
RookieAND <gwangin1999@naver.com>
2024-07-27 12:48:23 +0900

    feat: 컬러 토큰 정의

---
+++ src/styles/color.ts
@@ -0,0 +1,61 @@
+const blk = {
+    100: '#292B26',
+    90: '#3E403C',
+    80: '#545551',
+    70: '#696B67',
+    60: '#7F807D',
+    50: '#949592',
+    40: '#A9AAA8',
+    30: '#BFBFBE',
+    20: '#D4D5D4',
+    10: '#EAEAE9',
+} as const;
+
+const wht = '#FFFFFF' as const;
+
+const main = {
+    100: '#FF7512',
+    50: '#FFBA89',
+    30: '#FFD6B8',
+    10: '#FFF1E7',
+} as const;
+
+const warning = {
+    100: '#FF3B30',
+    10: '#FFEBEA',
+} as const;
+
+const good1 = {
+    100: '#1E82F1',
+    10: '#E9F3FE',
+} as const;
+
+const good2 = {
+    100: '#29BF6A',
+    10: '#EAF9F0',
+} as const;
+
+const sub1 = {
+    100: '#96A8FE',
+    50: '#CAD3FE',
+    30: '#E0E5FF',
+    10: '#F5F6FF',
+} as const;
+
+const sub2 = {
+    100: '#AED424',
+    50: '#E2F49D',
+    30: '#EDF9C5',
+    10: '#F9FDEB',
+} as const;
+
+export const COLOR = {
+    blk,
+    wht,
+    main,
+    warning,
+    good1,
+    good2,
+    sub1,
+    sub2,
+};
