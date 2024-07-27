a2b36d0709c84b38878eeface28e11ecaafa4998
RookieAND <gwangin1999@naver.com>
2024-07-27 13:24:49 +0900

    fix: 더 이상 사용하지 않는 preload link generator 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
-
-import { injectedPreloadLinkTags } from './src/styles/font-preload';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
         alias: [{ find: '#/', replacement: '/src/' }],
     },
-    plugins: [
-        react(),
-        createHtmlPlugin({
-            inject: {
-                tags: injectedPreloadLinkTags,
-            },
-        }),
-    ],
+    plugins: [react()],
     server: {
         proxy: {
             '/tmap': {
