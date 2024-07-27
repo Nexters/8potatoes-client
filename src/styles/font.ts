c55614df2f7260afd18aaca6a87c0226271ce0c6
RookieAND <gwangin1999@naver.com>
2024-07-27 12:48:31 +0900

    feat: 폰트 Weight 토큰 정의

---
+++ src/styles/font.ts
@@ -0,0 +1,13 @@
+export const FONT = {
+    suit: 'SUIT Variable',
+}
+
+export const FONT_WEIGHT = {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+    extraBold: 800,
+    black: 900,
+} as const;
