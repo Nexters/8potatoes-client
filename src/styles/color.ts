cc56d5b757ba1bfe22aaf0d3bda780c7aba6bc2f
Gwangin Baik <gwangin1999@naver.com>
2024-08-03 00:54:50 +0900

    [SETTING] 변경된 디자인 시안에 맞춰 Color Token 수정 (#4)

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,17 +1,20 @@
 const blk = {
-    100: '#292B26',
-    90: '#3E403C',
-    80: '#545551',
-    70: '#696B67',
-    60: '#7F807D',
-    50: '#949592',
-    40: '#A9AAA8',
-    30: '#BFBFBE',
-    20: '#D4D5D4',
-    10: '#EAEAE9',
+    100: '#201404',
+    90: '#362B1C',
+    80: '#4D4234',
+    70: '#63594C',
+    60: '#797165',
+    50: '#90887D',
+    40: '#A69F95',
+    30: '#BCB6AD',
+    20: '#D2CEC6',
+    10: '#E9E5DE',
+    5: '#F4F0EA'
 } as const;
 
-const wht = '#FFFFFF' as const;
+const wht = {
+    100: '#FFFFFF'
+} as const;
 
 const main = {
     100: '#FF7512',
@@ -20,42 +23,41 @@ const main = {
     10: '#FFF1E7',
 } as const;
 
-const warning = {
-    100: '#FF3B30',
-    10: '#FFEBEA',
-} as const;
-
-const good1 = {
-    100: '#1E82F1',
-    10: '#E9F3FE',
-} as const;
-
-const good2 = {
-    100: '#29BF6A',
-    10: '#EAF9F0',
+const bg = {
+    100: '#EAE0D6',
+    50: '#F7EDE1',
+    30: '#FFFCF6',
 } as const;
 
 const sub1 = {
     100: '#96A8FE',
     50: '#CAD3FE',
     30: '#E0E5FF',
-    10: '#F5F6FF',
+    10: '#F5F6FF'
+}
+
+const good = {
+    100: '#1E82F1',
 } as const;
 
-const sub2 = {
-    100: '#AED424',
-    50: '#E2F49D',
-    30: '#EDF9C5',
-    10: '#F9FDEB',
+const select = {
+    100: '#1E82F1',
 } as const;
+
+const error = {
+    100: '#FF3B30',
+} as const;
+
+const naver = '#34C11D';
 
 export const COLOR = {
     blk,
     wht,
     main,
-    warning,
-    good1,
-    good2,
+    error,
+    bg,
     sub1,
-    sub2,
+    good,
+    select,
+    naver,
 };
