e03db150dbcf392afb6b7de6d4b46399815585d5
RookieAND <gwangin1999@naver.com>
2024-08-05 19:48:32 +0900

    feat: 여정의 시작과 끝을 표기하는 DestinationIndicator 컴포넌트 추가

---
+++ src/features/rest-area/destination-indicator/index.ts
@@ -0,0 +1,1 @@
+export { DestinationIndicator } from './DestinationIndicator';
--- src/pages/rest-area-map/index.tsx
+++ src/pages/rest-area-map/index.tsx
@@ -2,9 +2,12 @@ import { type PropsWithChildren } from 'react';
 
 import { NaverMap, Container as NaverMapContainer } from 'react-naver-maps';
 
+import { DestinationIndicator } from '#/features/rest-area/destination-indicator';
+
 const RestAreaMapPage = ({ children }: PropsWithChildren<unknown>) => {
     return (
         <NaverMapContainer style={{ height: '100dvh' }}>
+            <DestinationIndicator start="서울" end="부산" />
             <NaverMap>{children}</NaverMap>
         </NaverMapContainer>
     );
