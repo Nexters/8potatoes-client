c9e42ce626e5b2107bae17d02842968cf9292a9c
RookieAND <gwangin1999@naver.com>
2024-08-06 00:42:15 +0900

    feat: 휴게소 위치를 지정하는 RestAreaBubbleMarker 컴포넌트 제작

---
+++ src/features/rest-area/rest-area-bubble-marker/RestAreaBubbleMarker.tsx
@@ -0,0 +1,9 @@
+import * as S from './RestAreaBubbleMarker.style';
+
+export const RestAreaBubbleMarker = () => {
+    return (
+        <S.Container>
+            <S.RestAreaName>천안삼거리(부산) 휴게소</S.RestAreaName>
+        </S.Container>
+    );
+};
