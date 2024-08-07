f70a126abd1d7d6972a9d26d127b7e558bd2715f
RookieAND <gwangin1999@naver.com>
2024-08-08 01:06:16 +0900

    hotfix: 새롭게 정의된 DS 에 맞춰 Typography 토큰 변경

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -3,22 +3,22 @@ export const FONT_FAMILY = {
 };
 
 export const FONT_STYLE_NAME = {
-    heading1: 'heading1',
-    heading2: 'heading2',
-    heading3: 'heading3',
-    heading4: 'heading4',
-    body1: 'body1',
-    body2: 'body2',
-    body3: 'body3',
-    body4: 'body4',
-    body5: 'body5',
-    body6: 'body6',
-    body7: 'body7',
-    smallText1: 'smallText1',
-    smallText2: 'smallText2',
-    button1: 'button1',
-    button2: 'button2',
-    caption: 'caption',
+    headingBold24: 'headingBold24',
+    headingBold20: 'headingBold20',
+    headingBold18: 'headingBold18',
+    headingSemiBold20: 'headingSemiBold20',
+    bodyBold16: 'bodyBold16',
+    bodyBold14: 'bodyBold14',
+    bodySemiBold18: 'bodySemiBold18',
+    bodySemiBold16: 'bodySemiBold16',
+    bodySemiBold14: 'bodySemiBold14',
+    bodyMedium16: 'bodyMedium16',
+    bodyMedium14: 'bodyMedium14',
+    buttonBold16: 'buttonBold16',
+    buttonBold14: 'buttonBold14',
+    captionSemiBold12: 'captionSemiBold12',
+    smallTextBold12: 'smallTextBold12',
+    smallTextMedium12: 'smallTextMedium12',
 } as const;
 
 const LINE_HEIGHT_RATIO = 1.5;
@@ -30,82 +30,82 @@ export const FONT_WEIGHT = {
 } as const;
 
 export const TYPOGRAPHY = {
-    [FONT_STYLE_NAME.heading1]: {
+    [FONT_STYLE_NAME.headingBold24]: {
         fontSize: '24px',
         lineHeight: `${LINE_HEIGHT_RATIO * 24}px`,
         fontWeight: FONT_WEIGHT.bold,
     },
-    [FONT_STYLE_NAME.heading2]: {
+    [FONT_STYLE_NAME.headingBold20]: {
         fontSize: '20px',
         lineHeight: `${LINE_HEIGHT_RATIO * 20}px`,
         fontWeight: FONT_WEIGHT.bold,
     },
-    [FONT_STYLE_NAME.heading3]: {
+    [FONT_STYLE_NAME.headingSemiBold20]: {
         fontSize: '20px',
         lineHeight: `${LINE_HEIGHT_RATIO * 20}px`,
         fontWeight: FONT_WEIGHT.semiBold,
     },
-    [FONT_STYLE_NAME.heading4]: {
+    [FONT_STYLE_NAME.headingBold18]: {
         fontSize: '18px',
         lineHeight: `${LINE_HEIGHT_RATIO * 18}px`,
         fontWeight: FONT_WEIGHT.bold,
     },
-    [FONT_STYLE_NAME.body1]: {
+    [FONT_STYLE_NAME.bodySemiBold18]: {
         fontSize: '18px',
         lineHeight: `${LINE_HEIGHT_RATIO * 18}px`,
         fontWeight: FONT_WEIGHT.semiBold,
     },
-    [FONT_STYLE_NAME.body2]: {
+    [FONT_STYLE_NAME.bodyBold16]: {
         fontSize: '16px',
         lineHeight: `${LINE_HEIGHT_RATIO * 16}px`,
         fontWeight: FONT_WEIGHT.bold,
     },
-    [FONT_STYLE_NAME.body3]: {
+    [FONT_STYLE_NAME.bodySemiBold16]: {
         fontSize: '16px',
         lineHeight: `${LINE_HEIGHT_RATIO * 16}px`,
         fontWeight: FONT_WEIGHT.semiBold,
     },
-    [FONT_STYLE_NAME.body4]: {
+    [FONT_STYLE_NAME.bodyMedium16]: {
         fontSize: '16px',
         lineHeight: `${LINE_HEIGHT_RATIO * 16}px`,
         fontWeight: FONT_WEIGHT.medium,
     },
-    [FONT_STYLE_NAME.body5]: {
+    [FONT_STYLE_NAME.bodyBold14]: {
         fontSize: '14px',
         lineHeight: `${LINE_HEIGHT_RATIO * 14}px`,
         fontWeight: FONT_WEIGHT.bold,
     },
-    [FONT_STYLE_NAME.body6]: {
+    [FONT_STYLE_NAME.bodySemiBold14]: {
         fontSize: '14px',
         lineHeight: `${LINE_HEIGHT_RATIO * 14}px`,
         fontWeight: FONT_WEIGHT.semiBold,
     },
-    [FONT_STYLE_NAME.body7]: {
+    [FONT_STYLE_NAME.bodyMedium14]: {
         fontSize: '14px',
         lineHeight: `${LINE_HEIGHT_RATIO * 14}px`,
         fontWeight: FONT_WEIGHT.medium,
     },
-    [FONT_STYLE_NAME.button1]: {
+    [FONT_STYLE_NAME.buttonBold16]: {
         fontSize: '16px',
         lineHeight: `${LINE_HEIGHT_RATIO * 16}px`,
         fontWeight: FONT_WEIGHT.bold,
     },
-    [FONT_STYLE_NAME.button2]: {
+    [FONT_STYLE_NAME.buttonBold14]: {
         fontSize: '14px',
         lineHeight: `${LINE_HEIGHT_RATIO * 14}px`,
         fontWeight: FONT_WEIGHT.bold,
     },
-    [FONT_STYLE_NAME.caption]: {
+    [FONT_STYLE_NAME.captionSemiBold12]: {
         fontSize: '12px',
         lineHeight: `${LINE_HEIGHT_RATIO * 12}px`,
         fontWeight: FONT_WEIGHT.semiBold,
     },
-    [FONT_STYLE_NAME.smallText1]: {
+    [FONT_STYLE_NAME.smallTextBold12]: {
         fontSize: '12px',
         lineHeight: `${LINE_HEIGHT_RATIO * 12}px`,
-        fontWeight: FONT_WEIGHT.semiBold,
+        fontWeight: FONT_WEIGHT.bold,
     },
-    [FONT_STYLE_NAME.smallText2]: {
+    [FONT_STYLE_NAME.smallTextMedium12]: {
         fontSize: '12px',
         lineHeight: `${LINE_HEIGHT_RATIO * 12}px`,
         fontWeight: FONT_WEIGHT.medium,
