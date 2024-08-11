87102bedb91901266235d4b16fe03e008e68f87c
RookieAND <gwangin1999@naver.com>
2024-08-11 23:05:13 +0900

    feat: 경로 명이 길 경우 truncate 처리

--- src/features/rest-area/destination-indicator/DestinationIndicator.style.tsx
+++ src/features/rest-area/destination-indicator/DestinationIndicator.style.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+import { Text } from '#/components/text';
 import { theme } from '#/styles/theme';
 
 export const Container = styled.div`
@@ -13,18 +14,22 @@ export const Container = styled.div`
 
     width: calc(100% - 40px);
     height: 48px;
+    padding: 0 16px;
 
     background-color: ${theme.color.wht[100]};
     border-radius: 16px;
     border: 1px solid ${theme.color.main[50]};
 `;
 
-export const Destination = styled.p`
+export const Destination = styled.div`
     flex-grow: 1;
-
     text-align: center;
 
-    color: ${theme.color.blk[60]};
-    font-weight: ${theme.font.weight.medium};
-    font-size: 16px;
-`
+    max-width: 131px;
+`;
+
+export const DestinationText = styled(Text)`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`;
--- src/features/rest-area/destination-indicator/DestinationIndicator.tsx
+++ src/features/rest-area/destination-indicator/DestinationIndicator.tsx
@@ -1,4 +1,5 @@
 import ArrowRightIcon from '#/assets/icons/arrow-right.svg?react';
+import { Text } from '#/components/text';
 import { theme } from '#/styles/theme';
 
 import * as S from './DestinationIndicator.style';
@@ -8,12 +9,23 @@ interface DestinationIndicatorProps {
     end: string;
 }
 
-export const DestinationIndicator = ({ start, end }: DestinationIndicatorProps) => {
+export const DestinationIndicator = ({
+    start,
+    end,
+}: DestinationIndicatorProps) => {
     return (
         <S.Container>
-            <S.Destination>{start.toString()}</S.Destination>
+            <S.Destination>
+                <S.DestinationText typography="bodyMedium16" color={theme.color.blk[60]}>
+                    {start}
+                </S.DestinationText>
+            </S.Destination>
             <ArrowRightIcon color={theme.color.main[50]} />
-            <S.Destination>{end.toString()}</S.Destination>
+            <S.Destination>
+                <S.DestinationText typography="bodyMedium16" color={theme.color.blk[60]}>
+                    {end}
+                </S.DestinationText>
+            </S.Destination>
         </S.Container>
     );
 };
