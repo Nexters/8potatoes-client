4cd1fab0b40381ca95922a78118927faf0d17afb
RookieAND <gwangin1999@naver.com>
2024-08-13 18:02:12 +0900

    feat: 휴게소 상세 정보와 연관된 타입 정의

---
+++ src/apis/rest-area/type.ts
@@ -0,0 +1,34 @@
+import type { RestAreaMenuDataType } from '#/types/menu';
+import type {
+    RestAreaAdditionalDataType,
+    RestAreaDetailAtHighwayType,
+    RestAreaGasStationDataType,
+    RestAreaParkingDataType,
+} from '#/types/rest-area';
+
+export interface HighwayRestAreaListParams {
+    from: string;
+    to: string;
+    roadNames: string;
+}
+
+export interface HighwayRestAreaListResponse {
+    totalReststopCount: number;
+    reststops: RestAreaDetailAtHighwayType[];
+}
+
+export interface RestAreaDetailInfoParams {
+    reststopCode: string;
+}
+
+export interface RestAreaDetailInfoResponse {
+    name: string; // 휴게소 이름
+    isOperating: boolean; // 식당 운영중 여부
+    startTime?: string; // 식당 운영 시작 시간 (옵션 필드)
+    endTime?: string; // 식당 운영 종료 시간 (옵션 필드)
+    naverRating?: number; // 네이버 평점 (옵션 필드, Float 대신 number 사용)
+    gasStationData: RestAreaGasStationDataType; // 주유소 정보
+    parkingData: RestAreaParkingDataType; // 주차 정보
+    reststopData: RestAreaAdditionalDataType; // 휴게소 기타 정보
+    menuData: RestAreaMenuDataType[]; // 메뉴 데이터 리스트
+}
---
+++ src/types/rest-area.ts
@@ -0,0 +1,46 @@
+import { AmenityType } from "./amenity";
+import { BrandType } from "./brand";
+import { MenuDataType, RepresentativeMenuDataType } from "./menu";
+import { RestaurantInfoType } from "./restaurant";
+
+export interface RestAreaDetailAtHighwayType {
+    name: string;
+    code: string;
+    isOperating: boolean;
+    gasolinePrice?: string;
+    dieselPrice?: string;
+    lpgPrice?: string;
+    naverRating: number;
+    foodMenusCount: number;
+}
+
+export interface RestAreaGasStationDataType {
+    gasolinePrice?: string;
+    dieselPrice?: string;
+    lpgPrice?: string;
+    isElectricChargingStation: boolean;
+    isHydrogenChargingStation: boolean;
+}
+
+export interface RestAreaParkingDataType {
+    smallCarSpace: number;
+    largeCarSpace: number;
+    disabledPersonSpace: number;
+    totalSpace: number;
+    updateDate: string;
+}
+
+export interface RestAreaAdditionalDataType {
+    restaurantOperatingTimes: RestaurantInfoType[]; // 운영 시간 리스트
+    brands: BrandType[]; // 브랜드 데이터 리스트
+    amenities: AmenityType[]; // 편의시설 데이터 리스트
+    address: string; // 주소
+    phoneNumber: string; // 전화번호
+}
+
+export interface RestAreaMenuDataType {
+    representativeMenuData: RepresentativeMenuDataType[]; // 대표 메뉴 데이터 (최대 2개)
+    totalMenuCount: number; // 전체 메뉴 개수
+    recommendedMenuData: MenuDataType[]; // 추천 메뉴 데이터 리스트
+    normalMenuData: MenuDataType[]; // 일반 메뉴 데이터 리스트
+}
