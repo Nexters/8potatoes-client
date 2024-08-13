c2bbf6c5cfe4e1ff1bf3334484664a2c8b9e881c
RookieAND <gwangin1999@naver.com>
2024-08-14 00:41:01 +0900

    feat: 고속도로 내 휴게소 목록을 가져오는 Query 의 enabled 옵션을 인계

--- src/query-hooks/rest-area/query.ts
+++ src/query-hooks/rest-area/query.ts
@@ -1,15 +1,29 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import {
+    type UseQueryOptions,
+    type UseSuspenseQueryOptions,
+    useQuery,
+    useSuspenseQuery,
+} from '@tanstack/react-query';
 
-import { getHighwayRestAreaList, getRestAreaDetailInfo } from '#/apis/rest-area';
-import { HighwayRestAreaListParams } from '#/apis/rest-area/type';
+import {
+    getHighwayRestAreaList,
+    getRestAreaDetailInfo,
+} from '#/apis/rest-area';
+import {
+    HighwayRestAreaListParams,
+    HighwayRestAreaListResponse,
+} from '#/apis/rest-area/type';
 import { REST_AREA_QUERY_KEY } from '#/constants/query-key';
 
-export const useGetHighwayRestAreaList = ({
-    from,
-    to,
-    roadNames,
-}: HighwayRestAreaListParams) => {
-    return useSuspenseQuery({
+export const useGetHighwayRestAreaList = (
+    { from, to, roadNames }: HighwayRestAreaListParams,
+    options: Omit<
+        UseQueryOptions<HighwayRestAreaListResponse>,
+        'queryKey' | 'queryFn'
+    >,
+) => {
+    return useQuery<HighwayRestAreaListResponse>({
+        ...options,
         queryKey: REST_AREA_QUERY_KEY.highwayList({ from, to, roadNames }),
         queryFn: () => getHighwayRestAreaList({ from, to, roadNames }),
     });
@@ -20,8 +34,8 @@ export const useGetRestAreaDetailInfo = (reststopCode: string) => {
         queryKey: REST_AREA_QUERY_KEY.detail(reststopCode),
         queryFn: () => getRestAreaDetailInfo({ reststopCode }),
         staleTime: 1000 * 60,
-    })
-}
+    });
+};
 
 export const useGetRestAreaGasStationInfo = (reststopCode: string) => {
     return useSuspenseQuery({
@@ -29,8 +43,8 @@ export const useGetRestAreaGasStationInfo = (reststopCode: string) => {
         queryFn: () => getRestAreaDetailInfo({ reststopCode }),
         select: (response) => response.gasStationData,
         staleTime: 1000 * 60,
-    })
-}
+    });
+};
 
 export const useGetRestAreaParkingInfo = (reststopCode: string) => {
     return useSuspenseQuery({
@@ -38,8 +52,8 @@ export const useGetRestAreaParkingInfo = (reststopCode: string) => {
         queryFn: () => getRestAreaDetailInfo({ reststopCode }),
         select: (response) => response.parkingData,
         staleTime: 1000 * 60,
-    })
-}
+    });
+};
 
 export const useGetRestAreaMenuInfo = (reststopCode: string) => {
     return useSuspenseQuery({
@@ -47,5 +61,5 @@ export const useGetRestAreaMenuInfo = (reststopCode: string) => {
         queryFn: () => getRestAreaDetailInfo({ reststopCode }),
         select: (response) => response.menuData,
         staleTime: 1000 * 60,
-    })
-}
+    });
+};
