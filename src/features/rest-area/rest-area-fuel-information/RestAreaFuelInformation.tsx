4a4406c8fd89564da964df0e1b57f92e50023b24
juhyojeong <juhyojeong58@gmail.com>
2024-08-15 19:22:42 +0900

    chore: 주유 및 충전 화면 수정

--- src/features/rest-area/rest-area-fuel-information/RestAreaFuelInformation.tsx
+++ src/features/rest-area/rest-area-fuel-information/RestAreaFuelInformation.tsx
@@ -21,84 +21,103 @@ export const RestAreaFuelInformation = ({
     isElectricChargingStation,
     isHydrogenChargingStation,
 }: RestAreaFuelInformationProps) => {
+    const hasNoFuelInfo = !gasolinePrice && !dieselPrice && !lpgPrice;
+
     return (
         <RestAreaDetailSection title="주유 및 충전" iconSrc="" iconAlt="">
-            <S.CardGroup row gap={12}>
-                {gasolinePrice && (
-                    <S.Card gap={20}>
-                        <Text
-                            color={theme.color.blk[60]}
-                            typography="bodyMedium16"
-                        >
-                            휘발유
-                        </Text>
-                        <Text
-                            color={theme.color.blk[100]}
-                            typography="headingBold18"
-                        >
-                            {gasolinePrice}
-                        </Text>
-                    </S.Card>
-                )}
-                {dieselPrice && (
-                    <S.Card gap={20}>
-                        <Text
-                            color={theme.color.blk[60]}
-                            typography="bodyMedium16"
-                        >
-                            경유
-                        </Text>
-                        <Text
-                            color={theme.color.blk[100]}
-                            typography="headingBold18"
-                        >
-                            {dieselPrice}
-                        </Text>
-                    </S.Card>
-                )}
-                {lpgPrice && (
-                    <S.Card gap={20}>
-                        <Text
-                            color={theme.color.blk[60]}
-                            typography="bodyMedium16"
-                        >
-                            LPG
-                        </Text>
-                        <Text
-                            color={theme.color.blk[100]}
-                            typography="headingBold18"
-                        >
-                            {lpgPrice}
-                        </Text>
-                    </S.Card>
-                )}
-            </S.CardGroup>
-            <FlexBox gap={20}>
-                <S.AvailableChargeOption row gap={12}>
-                    <Text color={theme.color.blk[60]} typography="bodyMedium16">
-                        전기차 충전
+            {hasNoFuelInfo ? (
+                <FlexBox gap={20} flexOption={{ alignItems: 'center' }}>
+                    <img src="" width={120} height={120} />
+                    <Text typography="bodyBold16" color={theme.color.blk[40]}>
+                        해당 휴게소의 주유소 정보가 없습니다.
                     </Text>
-                    <S.DashedLine />
-                    <Text
-                        color={theme.color.blk[100]}
-                        typography="bodySemiBold16"
-                    >
-                        {isElectricChargingStation ? '가능' : '불가능'}
-                    </Text>
-                </S.AvailableChargeOption>
-                <S.AvailableChargeOption row gap={12}>
-                    <Text color={theme.color.blk[60]} typography="bodyMedium16">
-                        수소차 충전
-                    </Text>
-                    <S.DashedLine />
-                    <Text
-                        color={theme.color.blk[100]}
-                        typography="bodySemiBold16"
-                    >
-                        {isHydrogenChargingStation ? '가능' : '불가능'}
-                    </Text>
-                </S.AvailableChargeOption>
-            </FlexBox>
+                </FlexBox>
+            ) : (
+                <>
+                    <S.CardGroup row gap={12}>
+                        {gasolinePrice && (
+                            <S.Card gap={20}>
+                                <Text
+                                    color={theme.color.blk[60]}
+                                    typography="bodyMedium16"
+                                >
+                                    휘발유
+                                </Text>
+                                <Text
+                                    color={theme.color.blk[100]}
+                                    typography="headingBold18"
+                                >
+                                    {gasolinePrice}
+                                </Text>
+                            </S.Card>
+                        )}
+                        {dieselPrice && (
+                            <S.Card gap={20}>
+                                <Text
+                                    color={theme.color.blk[60]}
+                                    typography="bodyMedium16"
+                                >
+                                    경유
+                                </Text>
+                                <Text
+                                    color={theme.color.blk[100]}
+                                    typography="headingBold18"
+                                >
+                                    {dieselPrice}
+                                </Text>
+                            </S.Card>
+                        )}
+                        {lpgPrice && (
+                            <S.Card gap={20}>
+                                <Text
+                                    color={theme.color.blk[60]}
+                                    typography="bodyMedium16"
+                                >
+                                    LPG
+                                </Text>
+                                <Text
+                                    color={theme.color.blk[100]}
+                                    typography="headingBold18"
+                                >
+                                    {lpgPrice}
+                                </Text>
+                            </S.Card>
+                        )}
+                    </S.CardGroup>
+                    <FlexBox gap={20}>
+                        <S.AvailableChargeOption row gap={12}>
+                            <Text
+                                color={theme.color.blk[60]}
+                                typography="bodyMedium16"
+                            >
+                                전기차 충전
+                            </Text>
+                            <S.DashedLine />
+                            <Text
+                                color={theme.color.blk[100]}
+                                typography="bodySemiBold16"
+                            >
+                                {isElectricChargingStation ? '가능' : '불가능'}
+                            </Text>
+                        </S.AvailableChargeOption>
+                        <S.AvailableChargeOption row gap={12}>
+                            <Text
+                                color={theme.color.blk[60]}
+                                typography="bodyMedium16"
+                            >
+                                수소차 충전
+                            </Text>
+                            <S.DashedLine />
+                            <Text
+                                color={theme.color.blk[100]}
+                                typography="bodySemiBold16"
+                            >
+                                {isHydrogenChargingStation ? '가능' : '불가능'}
+                            </Text>
+                        </S.AvailableChargeOption>
+                    </FlexBox>
+                </>
+            )}
         </RestAreaDetailSection>
     );
 };
