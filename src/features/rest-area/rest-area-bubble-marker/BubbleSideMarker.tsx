e7bd5effd0e669b33fe084c889579a0314f3e2e1
RookieAND <gwangin1999@naver.com>
2024-08-21 06:26:44 +0900

    feat: 말풍선 아이콘의 좌측, 우측 SVG 를 컴포넌트화

---
+++ src/features/rest-area/rest-area-bubble-marker/BubbleSideMarker.tsx
@@ -0,0 +1,159 @@
+import { theme } from "#/styles/theme";
+
+export const LeftSideBubbleMarker = () => (
+    <svg
+        width="13"
+        height="32"
+        viewBox="0 0 13 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <mask
+            id="mask0_1184_12593"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="0"
+            width="13"
+            height="32"
+        >
+            <path d="M0 0H13V32H0V0Z" fill={theme.color.wht[100]} />
+        </mask>
+        <g mask="url(#mask0_1184_12593)">
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8 0C3.58172 0 0 3.58172 0 8V24C0 28.4183 3.58172 32 8 32H150C154.418 32 158 28.4183 158 24V17.8169L163.901 6.46108C164.247 5.79539 163.764 5 163.013 5H157.418C156.232 2.06817 153.357 0 150 0H8Z"
+                fill="white"
+            />
+            <mask
+                id="mask1_1184_12593"
+                maskUnits="userSpaceOnUse"
+                x="0"
+                y="0"
+                width="165"
+                height="32"
+            >
+                <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M8 0C3.58172 0 0 3.58172 0 8V24C0 28.4183 3.58172 32 8 32H150C154.418 32 158 28.4183 158 24V17.8169L163.901 6.46108C164.247 5.79539 163.764 5 163.013 5H157.418C156.232 2.06817 153.357 0 150 0H8Z"
+                    fill="white"
+                />
+            </mask>
+            <g mask="url(#mask1_1184_12593)">
+                <path
+                    d="M158 17.8169L157.113 17.3559L157 17.5726V17.8169H158ZM157.418 5L156.492 5.37522L156.744 6H157.418V5ZM1 8C1 4.13401 4.13401 1 8 1V-1C3.02944 -1 -1 3.02944 -1 8H1ZM1 24V8H-1V24H1ZM8 31C4.134 31 1 27.866 1 24H-1C-1 28.9706 3.02943 33 8 33V31ZM150 31H8V33H150V31ZM157 24C157 27.866 153.866 31 150 31V33C154.971 33 159 28.9706 159 24H157ZM157 17.8169V24H159V17.8169H157ZM158.887 18.278L164.788 6.92217L163.013 5.99999L157.113 17.3559L158.887 18.278ZM164.788 6.92217C165.48 5.59077 164.514 4 163.013 4V5.99999L164.788 6.92217ZM163.013 4H157.418V6L163.013 5.99999V4ZM150 1C152.936 1 155.452 2.80802 156.492 5.37522L158.345 4.62478C157.011 1.32832 153.779 -1 150 -1V1ZM8 1H150V-1H8V1Z"
+                    fill={theme.color.main[100]}
+                />
+            </g>
+        </g>
+    </svg>
+);
+
+export const RightSideBubbleMarker = () => (
+    <svg
+        width="19"
+        height="32"
+        viewBox="0 0 19 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <mask
+            id="mask0_1184_12602"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="0"
+            width="19"
+            height="32"
+        >
+            <path d="M19 0H0V32H19V0Z" fill={theme.color.wht[100]} />
+        </mask>
+        <g mask="url(#mask0_1184_12602)">
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M-137 0C-141.418 0 -145 3.58172 -145 8V24C-145 28.4183 -141.418 32 -137 32H5C9.418 32 13 28.4183 13 24V17.8169L18.901 6.46108C19.247 5.79539 18.764 5 18.013 5H12.418C11.232 2.06817 8.35699 0 5 0H-137Z"
+                fill="white"
+            />
+            <mask
+                id="mask1_1184_12602"
+                maskUnits="userSpaceOnUse"
+                x="-145"
+                y="0"
+                width="165"
+                height="32"
+            >
+                <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M-137 0C-141.418 0 -145 3.58172 -145 8V24C-145 28.4183 -141.418 32 -137 32H5C9.418 32 13 28.4183 13 24V17.8169L18.901 6.46108C19.247 5.79539 18.764 5 18.013 5H12.418C11.232 2.06817 8.35699 0 5 0H-137Z"
+                    fill="white"
+                />
+            </mask>
+            <g mask="url(#mask1_1184_12602)">
+                <path
+                    d="M13 17.8169L12.113 17.3559L12 17.5726V17.8169H13ZM12.418 5L11.492 5.37522L11.744 6H12.418V5ZM-144 8C-144 4.13401 -140.866 1 -137 1V-1C-141.971 -1 -146 3.02944 -146 8H-144ZM-144 24V8H-146V24H-144ZM-137 31C-140.866 31 -144 27.866 -144 24H-146C-146 28.9706 -141.971 33 -137 33V31ZM5 31H-137V33H5V31ZM12 24C12 27.866 8.866 31 5 31V33C9.97099 33 14 28.9706 14 24H12ZM12 17.8169V24H14V17.8169H12ZM13.887 18.278L19.788 6.92217L18.013 5.99999L12.113 17.3559L13.887 18.278ZM19.788 6.92217C20.48 5.59077 19.514 4 18.013 4V5.99999L19.788 6.92217ZM18.013 4H12.418V6L18.013 5.99999V4ZM5 1C7.936 1 10.452 2.80802 11.492 5.37522L13.345 4.62478C12.011 1.32832 8.77901 -1 5 -1V1ZM-137 1H5V-1H-137V1Z"
+                    fill={theme.color.main[100]}
+                />
+            </g>
+        </g>
+    </svg>
+);
+
+export const LeftSideRecommendMarker = () => (
+    <svg
+        width="13"
+        height="32"
+        viewBox="0 0 13 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <mask
+            id="mask0_1184_12611"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="0"
+            width="13"
+            height="32"
+        >
+            <path d="M13 0H0V32H13V0Z" fill={theme.color.wht[100]} />
+        </mask>
+        <g mask="url(#mask0_1184_12611)">
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8 0C3.58172 0 0 3.58172 0 8V24C0 28.4183 3.58171 32 7.99999 32H157.856C162.275 32 165.856 28.4183 165.856 24V18.0922L171.9 6.46108C172.246 5.79539 171.763 5 171.013 5H165.275C164.088 2.06817 161.214 0 157.856 0H8Z"
+                fill={theme.color.main[100]}
+            />
+        </g>
+    </svg>
+);
+
+export const RightSideRecommendMarker = () => (
+    <svg
+        width="19"
+        height="32"
+        viewBox="0 0 19 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <mask
+            id="mask0_1184_12616"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="0"
+            width="19"
+            height="32"
+        >
+            <path d="M19 0H0V32H19V0Z" fill={theme.color.wht[100]} />
+        </mask>
+        <g mask="url(#mask0_1184_12616)">
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M-145 0C-149.418 0 -153 3.58172 -153 8V24C-153 28.4183 -149.418 32 -145 32H4.856C9.27501 32 12.856 28.4183 12.856 24V18.0922L18.9 6.46108C19.246 5.79539 18.763 5 18.013 5H12.275C11.088 2.06817 8.214 0 4.856 0H-145Z"
+                fill={theme.color.main[100]}
+            />
+        </g>
+    </svg>
+);
