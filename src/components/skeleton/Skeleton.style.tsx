eb4c61f4578cce7ac32832f15283cdfd39e2413b
RookieAND <gwangin1999@naver.com>
2024-08-22 01:23:16 +0900

    feat: Skeleton 공용 컴포넌트 추가

---
+++ src/components/skeleton/Skeleton.style.tsx
@@ -0,0 +1,26 @@
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { theme } from '#/styles/theme';
+
+export const shine = keyframes`
+    to {
+        background-position: right -4rem top 0;
+    }
+`;
+
+export const Container = styled.div`
+    background-color: ${theme.color.blk[20]};
+    border-radius: 4px;
+
+    background-image: linear-gradient(
+        90deg,
+        ${theme.color.blk[20]},
+        ${theme.color.blk[10]},
+        ${theme.color.blk[20]}
+    );
+    background-position: left -4rem top 0;
+    background-size: 4rem 100%;
+    background-repeat: no-repeat;
+    animation: ${shine} 1.5s infinite ease-in-out;
+`;
